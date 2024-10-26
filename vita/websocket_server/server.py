93d5d2752e09b0e09de8bbdf7b3a407139512adf
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2024-10-26 15:57:12 +0200

    add websocket server Helm chart and update deployment configuration

--- vita/websocket_server/server.py
+++ vita/websocket_server/server.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from websockets import ConnectionClosedOK
 from websockets.asyncio.server import serve
@@ -7,12 +8,15 @@ from vita.utils.config import WEBSOCKET_HOST, WEBSOCKET_PORT
 
 clients = set()
 
+logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler()])
+logger = logging.getLogger(__name__)
+
 
 async def handler(websocket):
     try:
         clients.add(websocket)
         async for message in websocket:
-            print(f"Received: {message}")
+            logger.info(f"Received: {message}")
             for client in clients:
                 if client != websocket:
                     await client.send(message)
@@ -24,6 +28,8 @@ async def handler(websocket):
 
 async def main():
     async with serve(handler, WEBSOCKET_HOST, WEBSOCKET_PORT):
+        logger.info(
+            f"Server started at ws://{WEBSOCKET_HOST}:{WEBSOCKET_PORT}")
         await asyncio.get_running_loop().create_future()  # run forever
 
 asyncio.run(main())
